93b7d17633c1158c5f054d1fb069f7108d2a59d1
CodeWracker <dev.rodrigofs@gmail.com>
2023-02-04 14:02:08 -0300

    fiz uma estrutura basica para a lista de projetos

--- src/components/ProjectsList/index.jsx
+++ src/components/ProjectsList/index.jsx
@@ -3,17 +3,35 @@ import { Card, CardHeader, CardBody, CardTitle, CardText, Button, Container } fr
 import data from '../../data.json'; // Arquivo JSON com os dados dos projetos
 import './projectsList.css';
 function ProjectsList() {
-    // Estado para armazenar os projetos filtrados
-    const [filteredProjects, setFilteredProjects] = useState([]);
+    const [allProjects, setAllProjects] = useState(data.projectsData.projectsList);
 
     // Estado para armazenar as tags selecionadas para filtrar
-    const [selectedTags, setSelectedTags] = useState([]);
+    const [allTags, setAllTags] = useState(data.projectsData.tags);
 
 
 
     return (
         <Container className="page-section projects-list-container">
             <h1 className="projects-list-title">Projects</h1>
+            <Container className="projects-list-grid-container">
+                {allProjects.map((project) => (
+                    <Card key={project.name} className="project-card">
+                        <CardHeader className="project-card-header">
+                            <img src={project.image} alt="logo" className="project-card-logo" />
+                            <h2 className="project-card-title">{project.name}</h2>
+                        </CardHeader>
+                        <CardBody className="project-card-body">
+                            <CardTitle className="project-card-subtitle">{project.link}</CardTitle>
+                            <CardText className="project-card-description">{project.description}</CardText>
+                            <div className="project-card-tags">
+                                {project.tags.map((tag) => (
+                                    <Button key={tag} className="project-card-tag" color="primary" outline>{allTags[tag]}</Button>
+                                ))}
+                            </div>
+                        </CardBody>
+                    </Card>
+                ))}
+            </Container>
         </Container>
     );
 }
