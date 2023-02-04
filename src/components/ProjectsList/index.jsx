051fc1b286cec914599d36035cb776a291a94b07
CodeWracker <dev.rodrigofs@gmail.com>
2023-02-04 16:02:23 -0300

    fiz o filtro com as tags

--- src/components/ProjectsList/index.jsx
+++ src/components/ProjectsList/index.jsx
@@ -7,14 +7,59 @@ function ProjectsList() {
 
     // Estado para armazenar as tags selecionadas para filtrar
     const [allTags, setAllTags] = useState(data.projectsData.tags);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [showFilter, setShowFilter] = useState(false);
+    const [filteredProjects, setFilteredProjects] = useState([]);
+
+    useEffect(() => {
+        setFilteredProjects(allProjects);
+    }, []);
+    useEffect(() => {
+        // console.log(selectedTags);
+        if (selectedTags.length === 0) {
+            setFilteredProjects(allProjects);
+        } else {
+            setFilteredProjects(allProjects.filter((project) => {
+                return project.tags.some((tag) => selectedTags.includes(tag));
+            }));
+        }
+
+        // console.log(filteredProjects);
+    }, [selectedTags]);
 
 
-
+    function handleSelectTag(tag) {
+        if (selectedTags.includes(tag)) {
+            setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    }
     return (
         <Container className="page-section projects-list-container">
             <h1 className="projects-list-title">Projects</h1>
+            <Container className="tag-filter-container">
+                <Button
+                    onClick={() => {
+                        setShowFilter(!showFilter);
+                    }}
+                    className="tag-filter-btn">Select Tags</Button>
+                {showFilter && (
+                    <Container className="tag-filter-list-container">
+                        {allTags.map((tag, index) => (
+                            <Button onClick={() => handleSelectTag(index)}
+                                key={tag} className={`tag-filter-item  ${selectedTags.includes(index) ? 'selected-tag' : 'disabled-tag'}`}
+                            >
+                                {tag}
+                            </Button>
+                        ))}
+                    </Container>
+                )}
+
+
+            </Container>
             <Container className="projects-list-grid-container">
-                {allProjects.map((project) => (
+                {filteredProjects.map((project) => (
                     <Card key={project.name} className="project-card">
                         <CardHeader className="project-card-header">
                             <img src={project.image} alt="logo" className="project-card-logo" />
@@ -25,7 +70,9 @@ function ProjectsList() {
                             <CardText className="project-card-description">{project.description}</CardText>
                             <div className="project-card-tags">
                                 {project.tags.map((tag) => (
-                                    <Button key={tag} className="project-card-tag" color="primary" outline>{allTags[tag]}</Button>
+                                    <Button
+                                        key={tag} className="project-card-tag"
+                                        color="primary" outline>{allTags[tag]}</Button>
                                 ))}
                             </div>
                         </CardBody>
